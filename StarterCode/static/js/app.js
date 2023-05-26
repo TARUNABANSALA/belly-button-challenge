2756a12188e6d52ed6b3c2b1328f72ffc650e6d8
TARUNABANSALA <124708259+TARUNABANSALA@users.noreply.github.com>
2023-05-26 16:59:46 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,97 @@
-npm init -y
-
-// Import the D3 library
-import * as d3 from 'd3'
-
 // Define the URL for the JSON file
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
 // Use D3 to make the AJAX request
-d3.json(url).then(function(data) {
-  // Access the JSON data
-  console.log(data);
-}).catch(function(error) {
-  // Handle any errors that occur during the request
-  console.error('Error loading the JSON file:', error);
+d3.json(url).then(data => {
+  const samples = data.samples;
+  // Get the top 10 OTUs
+  const top10OTUs = samples[0].sample_values.slice(0, 10);
+  const top10IDs = samples[0].otu_ids.slice(0, 10);
+  const top10Labels = samples[0].otu_labels.slice(0, 10);
+  // Create the trace object
+  const trace = {
+    x: top10OTUs,
+    y: top10IDs.map(id => `OTU ${id}`),
+    type: 'bar',
+    orientation: 'h',
+  };
+  // Create the data array
+  const top10data = [trace];
+
+  // Create the layout object
+  const layout = {
+    title: 'Top 10 OTUs',
+    xaxis: {
+      title: 'Sample Values',
+    },
+    yaxis: {
+      title: 'OTU IDs',
+      automargin: true,
+    },
+  };
+  // Plot the chart
+  Plotly.newPlot('plot', top10data, layout);
+}).catch(error => {
+  console.log('Error loading data:', error);
+});
+
+
+// Use D3 to make the AJAX request
+d3.json(url).then(data => {
+  const samples = data.samples;
+  const otuIDs = samples[0].otu_ids;
+  const sampleValues = samples[0].sample_values;
+  const otuLabels = samples[0].otu_labels;
+
+  // Create the trace object
+  const trace = {
+    x: otuIDs,
+    y: sampleValues,
+    text: otuLabels,
+    mode: 'markers',
+    marker: {
+      size: sampleValues,
+      color: otuIDs,
+      colorscale: 'Viridis',
+    },
+  };
+
+  // Create the data array
+  const bubbledata = [trace];
+
+  // Create the layout object
+  const layout = {
+    title: 'Bubble Chart',
+    xaxis: {
+      title: 'OTU IDs',
+    },
+    yaxis: {
+      title: 'Sample Values',
+    },
+    showlegend: false,
+  };
+
+  // Plot the chart
+  Plotly.newPlot('plot2', bubbledata, layout);
+}).catch(error => {
+  console.log('Error loading data:', error);
+});
+
+// Display the sample metadata, i.e., an individual's demographic information.
+d3.json(url).then(data => {
+  const metadata = data.metadata; // Access the metadata from the JSON response
+  console.log(metadata[0]);
+
+// Select the metadata element using its id
+  const metadataElement = d3.select("#sample-metadata");
+
+// Clear the existing contents (if any)
+  metadataElement.html("");
+
+// Iterate over the metadata object and append the information to the element
+  Object.entries(metadata[0]).forEach(([key, value]) => {
+    metadataElement
+      .append("p")
+      .text(`${key}: ${value}`);
+  })
 });
