b2f30a7f04af8110c5e1d5ad8b50abaea9e1ebc3
TARUNABANSALA <124708259+TARUNABANSALA@users.noreply.github.com>
2023-06-01 21:30:53 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,57 @@
 // Define the URL for the JSON file
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
-var urlData = '';
+
 loadOptions();
+// Display the sample metadata, i.e., an individual's demographic information.
 demographic(0);
-
 top10data();
+function loadOptions() {
+  d3.json(url).then(data => {
+    var options = data.names;
+    var select = document.getElementById("selDataset");
+    for (var i = 0; i < options.length; i++) {
+      var option = document.createElement("option");
+      option.text = options[i];
+      select.add(option);
+    }
+  }).catch(error => {
+    console.log('Error loading data:', error);
+  })
+}
 
+// Display the sample metadata, i.e., an individual's demographic information. Display each key-value pair from the metadata JSON object somewhere on the page.
+function demographic(selectedMetadataIndex) {
+  d3.json(url).then(data => {
+    const metadata = data.metadata; // Access the metadata from the JSON response
+    console.log(metadata[selectedMetadataIndex]);
+    // Select the metadata element using its id
+    const metadataElement = d3.select("#sample-metadata");
+    // Clear the existing contents (if any)
+    metadataElement.html("");
+    // Iterate over the metadata object and append the information to the element
+    Object.entries(metadata[selectedMetadataIndex]).forEach(([key, value]) => {
+      metadataElement
+        .append("p")
+        .text(`${key}: ${value}`);
+    });
+  });
+}
+// Update all the plots when a new sample is selected:
+function optionChanged(selectedId) {
+  d3.json(url).then(data => {
+    const metadata = data.metadata;
+    selectedMetadataIndex = findValue(metadata, parseInt(selectedId));
+    demographic(selectedMetadataIndex)});
+};
+
+function findValue(metadata, target) {
+  for (let i = 0; i < metadata.length; i++) {
+    if (metadata[i].id === target) {
+      return i; // Match found
+    }
+  }
+  return -1; // Match not found
+}
 function top10data() {
   // Use D3 to make the AJAX request
   d3.json(url).then(data => {
@@ -77,53 +123,3 @@ d3.json(url).then(data => {
 }).catch(error => {
   console.log('Error loading data:', error);
 });
-
-// Display the sample metadata, i.e., an individual's demographic information.
-function demographic(selectedMetadataIndex) {
-  d3.json(url).then(data => {
-    const metadata = data.metadata; // Access the metadata from the JSON response
-    console.log(metadata[selectedMetadataIndex]);
-    // Select the metadata element using its id
-    const metadataElement = d3.select("#sample-metadata");
-    // Clear the existing contents (if any)
-    metadataElement.html("");
-    // Iterate over the metadata object and append the information to the element
-    Object.entries(metadata[selectedMetadataIndex]).forEach(([key, value]) => {
-      metadataElement
-        .append("p")
-        .text(`${key}: ${value}`);
-    });
-  });
-}
-
-
-function loadOptions() {
-  d3.json(url).then(data => {
-    urlData = data;
-    var options = data.names;
-    var select = document.getElementById("selDataset");
-    for (var i = 0; i < options.length; i++) {
-      var option = document.createElement("option");
-      option.text = options[i];
-      select.add(option);
-    }
-  }).catch(error => {
-    console.log('Error loading data:', error);
-  })
-}
-
-function optionChanged(selectedId) {
-  selectedMetadataIndex = findValue(urlData.metadata, parseInt(selectedId));
-  demographic(selectedMetadataIndex);
-}
-
-
-function findValue(metadata, target) {
-  for (let i = 0; i < metadata.length; i++) {
-    if (metadata[i].id === target) {
-      return i; // Match found
-    }
-  }
-  return -1; // Match not found
-}
-
